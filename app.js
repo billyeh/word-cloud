6ace6329a52e590521a97d7aec569c65bab162a3
= <billwyeh@yahoo.com>
2013-10-18 22:07:04 -0700

    switch to jade, GAME ON

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '../public'));
 var twitterAPI = require('node-twitter-api');
 var twitter = new twitterAPI({
   consumerKey: 'xUVJ7OkUYZgtG9KK3DyA',
   consumerSecret: '4qr6qV24XzsBVR7bljRwLefFXo1ggigocvDrYpAKRs',
   callback: 'oob'
 });
+var jade = require('jade');
+var fn = jade.compile('./index.html');
 
 app.get('/', function(req, res) {
   var accToken = '829348675-dB86OcFHh4wZOAizD4YREA8TAGhlwYoP0pTXzzy1';
@@ -14,11 +18,11 @@ app.get('/', function(req, res) {
     if (error) {
       console.log(JSON.stringify(error));
     }
-    else {
+    else { // success!!
+      res.render('index.jade', {title: 'Franz Enzenhofer'});
       console.log(JSON.stringify(data));
     }
   });
-  res.end('hello');
 });
 
 app.listen(3000);

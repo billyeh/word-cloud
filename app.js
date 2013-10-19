4da954d60776d65496dc44e34021afc51fae490a
= <billwyeh@yahoo.com>
2013-10-18 21:07:56 -0700

    sorry about earlier, working commit for requestToken/Secret

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
+var twitterAPI = require('node-twitter-api');
+var twitter = new twitterAPI({
+  consumerKey: 'xUVJ7OkUYZgtG9KK3DyA',
+  consumerSecret: '4qr6qV24XzsBVR7bljRwLefFXo1ggigocvDrYpAKRs',
+  callback: 'oob'
+});
 
 app.get('/', function(req, res) {
-  client.get("https://api.twitter.com/1.1/search/tweets.json?q=bart%20strike", function(data, response){
-            // parsed response body as js object
-            console.log(data);
-            // raw response
-            console.log(response);
-        });
+  var reqToken;
+  var reqTokenSecret;
+  twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+    if (error) {
+      console.log("Error getting OAuth request token : " + JSON.stringify(error));
+    } else {
+      reqToken = requestToken;
+      reqTokenSecret = requestTokenSecret;
+    }
+  });
   res.end('hello');
 });
 

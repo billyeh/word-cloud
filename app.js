fadaacd7127533588070e6dcf8a01d8bd0c97738
= <billwyeh@yahoo.com>
2013-10-18 21:30:32 -0700

    working on oauth, added jade

--- app.js
+++ app.js
@@ -10,12 +10,29 @@ var twitter = new twitterAPI({
 app.get('/', function(req, res) {
   var reqToken;
   var reqTokenSecret;
+  var oauth_verifier = 6184592;
+  var accToken;
+  var accTokenSecret;
   twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
     if (error) {
       console.log("Error getting OAuth request token : " + JSON.stringify(error));
     } else {
       reqToken = requestToken;
       reqTokenSecret = requestTokenSecret;
+      console.log('Request token:' + reqToken);
+      console.log('Request token secret:' + reqTokenSecret);
+    }
+  });
+  twitter.getAccessToken(reqToken, reqTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+    if (error) {
+      console.log(JSON.stringify(error));
+    } else {
+      /*
+      accToken = accessToken;
+      accTokenSecret = accessTokenSecret;
+      console.log('Access token:' + accToken);
+      console.log('Access token secret:' + accTokenSecret);
+      */
     }
   });
   res.end('hello');
